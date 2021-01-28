573dc919e1c082fefc9a629723d82204cb0e5e39
The Hunger Project <john.coonrod@thp.org>
2021-01-28 16:20:09 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,14 +3,13 @@
  * The template for displaying the footer
  *
  */
-
 ?>
 
 </div>
 
-<footer class="site-footer">untheme by Tania Rascia.</footer>
-
-<?php wp_footer(); ?>
+<footer class="site-footer">
+  <a href=/policies>Policies & Disclosures</a> <a href=/press>Press & Media</a> <a href=/terms>Terms of Use</a>
+</footer>
 
 </body>
 </html>
