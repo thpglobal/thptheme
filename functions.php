6874a7ef0e17611552dff1d7eda2945f4cafd24d
The Hunger Project <john.coonrod@thp.org>
2021-01-28 16:44:09 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,10 +6,6 @@
 
 add_theme_support( 'title-tag' );
 add_theme_support( 'custom-logo' );
-
-/*
- * Enable support for Post Thumbnails on posts and pages.
- */
 add_theme_support( 'post-thumbnails' );
 
 /*
