13ad74bc90fceeecdb3fdc500e03a5bc339b5b19
The Hunger Project <john.coonrod@thp.org>
2021-02-06 17:52:13 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -4,22 +4,26 @@
  *
  */
 ?>
+</div> <!-- close the page container -->
+<div class="actionbar">
+		Take Action Now!
+</div>
+<footer class="site-footer">
 <div class="wp-block-columns">
-  <div class="wp-block-column">
-    Footer-1
+  <div class="wp-block-column footer1">
+	<?php dynamic_sidebar( 'footer_area_one' ); ?>
   </div>
-  <div class="wp-block-column">
-    Footer-2
+  <div class="wp-block-column footer2">
+	<?php dynamic_sidebar( 'footer_area_two' ); ?>
   </div>
-  <div class="wp-block-column">
-    Footer-3
+  <div class="wp-block-column footer3">
+	<?php dynamic_sidebar( 'footer_area_three' ); ?>
   </div>
 </div>
 
-</div> <!-- close the page container -->
-
-<footer class="site-footer">
-  <a href=/policies>Policies & Disclosures</a> <a href=/press>Press & Media</a> <a href=/terms>Terms of Use</a>
+<p>	  
+	<a href=/policies>Policies & Disclosures</a> | <a href=/press>Press & Media</a> | <a href=/terms>Terms of Use</a> | <a href=/wp-admin>Admin</a>
+	</p>
 </footer>
 
 </body>
