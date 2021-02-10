9337c6727e62ae1d2969fd48da1b8f813ccd6749
The Hunger Project <john.coonrod@thp.org>
2021-02-09 20:16:12 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  *
  */
 add_theme_support( 'title-tag' );
-add_theme_support( 'custom-logo' );
+add_theme_support( 'custom-logo',['height'=>125] );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'editor-color-palette',
     '#0000aa',
