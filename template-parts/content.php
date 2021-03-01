68fed307067f5effe603dd899f2ed76598584465
The Hunger Project <john.coonrod@thp.org>
2021-03-01 11:31:19 -0500

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -8,6 +8,7 @@
 <article id="post-<?php the_ID(); ?>" class="entry">
 	<header class="entry-header">
 		<h1><?php the_title(); ?></h1>
+		<h4><?php the_date(); ?></h4>
 		<?php if ( has_post_thumbnail() ) { 
 			$thpurl=get_the_post_thumbnail_url();
 			echo("<img src='$thpurl' width=100% height=auto>");
