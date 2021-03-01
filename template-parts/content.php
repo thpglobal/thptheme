58e78ac3dbd6bc052cc4885d92871a1675e04b62
The Hunger Project <john.coonrod@thp.org>
2021-03-01 12:27:33 -0500

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,15 +9,16 @@
 	<header class="entry-header">
 		<h1><?php the_title(); ?></h1>
 		<h4><?php the_date(); ?></h4>
-		<?php if ( has_post_thumbnail() ) { 
-			$thpurl=get_the_post_thumbnail_url();
-			echo("<img src='$thpurl' width=100% height=auto>");
-		} ?>
+		
 	</header>
 
 	<div class="entry-content wp-block-columns">
 	  <div class="wp-block-column" style="flex-basis:70%">
-		<?php the_content(); ?>
+		  <?php if ( has_post_thumbnail() ) { 
+			$thpurl=get_the_post_thumbnail_url();
+			echo("<img src='$thpurl' width=100% height=auto>");
+		} 
+		  the_content(); ?>
 		</div>
 	  <div class="wp-block-column" style="flex-basis:30%">
 		  <?php dynamic_sidebar( 'sidebar-1' );?>
