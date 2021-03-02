4dff436563c392b72774161402bb84eaf9c01327
The Hunger Project <john.coonrod@thp.org>
2021-03-01 21:15:08 -0500

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,11 @@
 
 // The animation function, which takes an Element
 const animateCountUp = el => {
+	const animationDuration = 2000;
+	const frameDuration = 1000 / 60;
+	const totalFrames = Math.round( animationDuration / frameDuration );
+	// An ease-out function that slows the count as it progresses
+	const easeOutQuad = t => t * ( 2 - t );
 	let frame = 0;
 	const countTo = parseInt( el.innerHTML, 10 );
 	const counter = setInterval( () => {
@@ -18,14 +23,6 @@ const animateCountUp = el => {
 
 // Run the animation on all elements with a class of ‘countup’
 const runAnimations = () => {
-	// Enable the countups
-	const animationDuration = 2000;
-	const frameDuration = 1000 / 60;
-	const totalFrames = Math.round( animationDuration / frameDuration );
-	// An ease-out function that slows the count as it progresses
-	const easeOutQuad = t => t * ( 2 - t );
-
 	const countupEls = document.querySelectorAll( '.countup' );
 	countupEls.forEach( animateCountUp );
 };
-window.onload=runAnimations;
