96662c0d89aac580b381f116c5be8baa7a7c4934
The Hunger Project <john.coonrod@thp.org>
2021-05-03 11:52:16 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,28 @@ get_header();
 
 <main id="main" class="site-main" role="main">
 
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		get_template_part( 'template-parts/content', get_post_type() );
+<article id="post-<?php the_ID(); ?>" class="entry">
+	<header class="entry-header">
+		<h1><?php the_title(); ?></h1>
+		<h4><?php the_date(); ?></h4>
+		
+	</header>
 
-	endwhile;
+	<div class="entry-content wp-block-columns">
+	  <div class="wp-block-column" style="flex-basis:70%">
+		  <?php if ( has_post_thumbnail() ) { 
+			$thpurl=get_the_post_thumbnail_url();
+			echo("<img src='$thpurl' width=100% height=auto>");
+		} 
+		  the_content(); ?>
+		</div>
+	  <div class="wp-block-column" style="flex-basis:30%">
+		  <?php dynamic_sidebar( 'sidebar-1' );?>
+	</div>
+</article>
+<?php	endwhile;
 
 	the_posts_pagination( array(
 		'prev_text' => __( 'Previous page' ),
